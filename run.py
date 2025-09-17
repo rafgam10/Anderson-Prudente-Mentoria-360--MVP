5a489aea7cb81daad0fa2070be2fc0815db64fcf
Rafgam <rafgam19@gmail.com>
2025-09-17 17:55:42 -0300

    Código run, com modifcações de conexão com o DB(SQLAlchemy) e configs do Flask-Login

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from app.routes import all_blueprints
+from flask_login import LoginManager
+from app.models import db, Administrador, Aluno
 
 app = Flask(
     __name__,
@@ -7,10 +9,29 @@ app = Flask(
     static_folder="app/static"
 )
 app.secret_key = '57ee3757ba351d795da0b100ce3bfb97d623fb24d2ce123380a4d3ee21d1e076'
+app.config.from_object('app.config')
+
+db.init_app(app)
+
+# Configuração do Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login.login_page'
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = Administrador.query.get(int(user_id))
+    if user:
+        return user
+    return Aluno.query.get(int(user_id))
+
 
 # Registra os blueprints
 for bp in all_blueprints:
     app.register_blueprint(bp)
+
 
 @app.route("/")
 def index():

e0825be0bb95d20f5ac6154b1cccb4007f1b7ce3
Rafgam <rafgam19@gmail.com>
2025-09-17 18:06:05 -0300

    Modificações do config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,5 @@ HOST = "localhost"
 DB_NAME = "mentoria360_mvp"
 
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DB_NAME}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False # Recommended to disable

80d1761e0dfca75454223721946bef8dacfb9e9e
Rafgam <rafgam19@gmail.com>
2025-09-18 17:11:15 -0300

    Adicionando o Blueprint alunoRoute.py no __init__.py.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from .loginRoute import login_bp
 from .adminRoute import admin_bp
+from .alunoRoute import aluno_bp
 
 all_blueprints = [login_bp]
 all_blueprints.append(admin_bp)
+all_blueprints.append(aluno_bp)
---
+++ app/routes/alunoRoute.py
@@ -0,0 +1,25 @@
+from flask import (
+    Blueprint,
+    render_template,
+    redirect,
+    url_for,
+    flash,
+    request
+)
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.models import db, Aluno
+
+aluno_bp = Blueprint("aluno", __name__,url_prefix='/aluno')
+
+
+@aluno_bp.route('/home', methods=["GET"])
+def home_index_aluno():
+    
+    # Aqui fica a página principal com uma Navbar no centro dos produtos.
+    return "Página do aluno"
+
+
+
+
+
+

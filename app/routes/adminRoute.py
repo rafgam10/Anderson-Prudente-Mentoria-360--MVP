9711a6792bdc8004bdbd4bab39b95f3f27b1cb98
Rafgam <rafgam19@gmail.com>
2025-09-17 22:25:10 -0300

    Rota de Cadastro de Administradores.

--- app/routes/adminRoute.py
+++ app/routes/adminRoute.py
@@ -6,12 +6,13 @@ from flask import (
     flash,
     request
 )
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.models import db, Administrador
 
 admin_bp = Blueprint("admin", __name__, url_prefix='/admin')
 
 
 #### Página Admin Principal
-
 @admin_bp.route('/', methods=["GET"])
 def index_admin():
     return render_template('baseAdm.html')
@@ -25,6 +26,31 @@ def lista_admins():
 
 @admin_bp.route('/admins/cadastro', methods=["GET","POST"])
 def cadastro_admins():
+    if request.method == "POST":
+        nomeAdminInput = request.form.get('nomeAdmin')
+        emailAdminInput = request.form.get('emailAdmin')
+        senhaAdminInput = generate_password_hash(request.form.get('senhaAdmin'))
+        
+        novo_Admin = Administrador(
+            nomeAdmin=nomeAdminInput, 
+            emailAdmin=emailAdminInput,
+            senhaAdmin=senhaAdminInput
+        )
+        print("Obj Admin criando...")
+        
+        try:
+            db.session.add(novo_Admin)
+            db.session.commit()
+            print("Commit OK!")
+        except Exception as e:
+            db.session.rollback()
+            print("Erro ao salvar:", e)
+
+        
+        flash("Novo Administrador criado com sucesso!", "success")
+        return redirect(url_for('admin.cadastro_admins'))
+    
+    # GET
     return render_template('cadastroAdmins.html')
 
 

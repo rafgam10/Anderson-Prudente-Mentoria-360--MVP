b6676a6eb163e8a032d36fe36f47a25366cfe4c4
Timoteo <timoteo@hdinfo-ma.com>
2025-11-26 21:42:34 -0300

    main: adição do Blueprint /mentorias e divs no navbar que está no baseAdm.html.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,9 @@
 from .loginRoute import login_bp
 from .adminRoute import admin_bp
 from .alunoRoute import aluno_bp
+from .mentoriaRoute import mentoria_bp
 
 all_blueprints = [login_bp]
 all_blueprints.append(admin_bp)
 all_blueprints.append(aluno_bp)
+all_blueprints.append(mentoria_bp)
---
+++ app/routes/mentoriaRoute.py
@@ -0,0 +1,24 @@
+from flask import (
+    Blueprint,
+    render_template,
+    url_for,
+    redirect,
+    jsonify,
+    request,
+    flash
+)
+
+
+
+mentoria_bp = Blueprint("mentoria", __name__, url_prefix="/mentorias")
+
+## Rotas de Mentorias:
+
+@mentoria_bp.route("/cadastrar-mentoria", methods=["GET", "POST"])
+def cadastrar_mentorias():
+    pass
+
+
+@mentoria_bp.route("/listar-mentoria", methods=["GET"])
+def listar_mentorias():
+    pass
